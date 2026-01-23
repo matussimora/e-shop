3d3cfad1ae1ee5c6f453fc16087e2a86af3fa894
Matus <matussimora10@gmail.com>
2026-01-23 09:46:49 +0100

    fhedge

--- e-shop/Program.cs
+++ e-shop/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Text;
-
+using System.Globalization;
 
 namespace HeartzClothing
 {
@@ -55,21 +57,119 @@ namespace HeartzClothing
 
     internal class SpravcaProduktov
     {
-        private string cestaProdukty;
+        private readonly string cestaProdukty;
 
         public SpravcaProduktov(string cestaProdukty)
         {
             this.cestaProdukty = cestaProdukty;
         }
 
-        internal List<Produkt> NacitajProdukty()
+        public List<Produkt> NacitajProdukty()
         {
-            throw new NotImplementedException();
+            var produkty = new List<Produkt>();
+
+            if (!File.Exists(cestaProdukty))
+            {
+                return produkty;
+            }
+
+            string[] riadky = File.ReadAllLines(cestaProdukty, Encoding.UTF8);
+
+            foreach (string riadok in riadky)
+            {
+                if (string.IsNullOrWhiteSpace(riadok))
+                {
+                    continue;
+                }
+
+                string[] casti = riadok.Split(';');
+
+                // Support two formats:
+                // 1) Legacy: id;nazov;kategoria;cena;mnozstvo
+                // 2) Extended (OblecenieProdukt): id;nazov;kategoria;cena;mnozstvo;farba;velkost
+                if (casti.Length < 5)
+                {
+                    continue;
+                }
+
+                if (!int.TryParse(casti[0], out int id))
+                {
+                    continue;
+                }
+
+                string nazov = casti[1];
+                string kategoria = casti[2];
+
+                if (!decimal.TryParse(casti[3], NumberStyles.Number, CultureInfo.InvariantCulture, out decimal cena))
+                {
+                    continue;
+                }
+
+                if (!int.TryParse(casti[4], out int mnozstvoNaSklade))
+                {
+                    continue;
+                }
+
+                if (casti.Length >= 7)
+                {
+                    // OblecenieProdukt with color and size
+                    var op = new OblecenieProdukt
+                    {
+                        Id = id,
+                        Nazov = nazov,
+                        Kategoria = kategoria,
+                        Cena = cena,
+                        MnozstvoNaSklade = mnozstvoNaSklade,
+                        Farba = casti[5],
+                        Velkost = casti[6]
+                    };
+                    produkty.Add(op);
+                }
+                else
+                {
+                    var p = new Produkt
+                    {
+                        Id = id,
+                        Nazov = nazov,
+                        Kategoria = kategoria,
+                        Cena = cena,
+                        MnozstvoNaSklade = mnozstvoNaSklade
+                    };
+                    produkty.Add(p);
+                }
+            }
+
+            return produkty;
         }
 
-        internal void UlozProdukty(List<Produkt> produkty)
+        public void UlozProdukty(List<Produkt> produkty)
         {
-            throw new NotImplementedException();
-        }
+            var riadky = new List<string>(produkty.Count);
+
+            foreach (var p in produkty)
+            {
+                string cena = p.Cena.ToString(CultureInfo.InvariantCulture);
+
+                if (p is OblecenieProdukt op)
+                {
+                    // Save extended format with color and size
+                    riadky.Add($"{op.Id};{op.Nazov};{op.Kategoria};{cena};{op.MnozstvoNaSklade};{op.Farba};{op.Velkost}");
+                }
+                else
+                {
+                    riadky.Add($"{p.Id};{p.Nazov};{p.Kategoria};{cena};{p.MnozstvoNaSklade}");
+                }
+            }
+
+            // Ensure directory creation handles empty or rootless paths
+            string dir = Path.GetDirectoryName(cestaProdukty);
+            if (string.IsNullOrWhiteSpace(dir))
+            {
+                dir = ".";
+            }
+
+            Directory.CreateDirectory(dir);
+            File.WriteAllLines(cestaProdukty, riadky, Encoding.UTF8);
+        
     }
 }
